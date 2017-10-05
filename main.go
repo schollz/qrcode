51c3b3f81f737dda3a2d4cb689ee15ab8b01c545
Zack Scholl <zack.scholl@gmail.com>
2017-10-05 11:24:42 -0600

    Add new dependencies

--- main.go
+++ main.go
@@ -3,21 +3,56 @@ package main
 import (
 	"fmt"
 	"io/ioutil"
-	"log"
+	"os"
+	"time"
 
+	"github.com/urfave/cli"
 	"rsc.io/qr"
 )
 
+var version string
+
 func main() {
-	b, err := ioutil.ReadFile("test.gpg")
+	app := cli.NewApp()
+	app.Name = "qrcode"
+	app.Usage = "generate a QR code from a text file"
+	app.Version = version
+	app.Compiled = time.Now()
+	app.Flags = []cli.Flag{
+		cli.StringFlag{
+			Name:  "file, f",
+			Value: "",
+			Usage: "text file to convert to QR code",
+		},
+		cli.StringFlag{
+			Name:  "output, o",
+			Value: "qr.png",
+			Usage: "file to output",
+		},
+	}
+
+	app.Action = func(c *cli.Context) error {
+		err := qrcode(c.GlobalString("file"), c.GlobalString("output"))
+		if err != nil {
+			fmt.Print(err)
+		} else {
+			fmt.Printf("QR code from '%s' written to '%s'\n", c.GlobalString("file"), c.GlobalString("output"))
+		}
+		return err
+	}
+
+	app.Run(os.Args)
+}
+
+func qrcode(readFile, outputFile string) (err error) {
+	b, err := ioutil.ReadFile(readFile)
+	if err != nil {
+		return
+	}
 	q, err := qr.Encode(string(b), qr.L)
 	if err != nil {
-		log.Fatal(err)
+		return
 	}
-	fmt.Println(q, err)
-	q.PNG()
-	err = ioutil.WriteFile("test.png", q.PNG(), 0644)
-	if err != nil {
-		log.Fatal(err)
-	}
+	err = ioutil.WriteFile(outputFile, q.PNG(), 0644)
+	return
 }
