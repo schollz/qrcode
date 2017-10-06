84b8ec7abadef98340bfa7f5db6fb8e7fa2c92b3
Zack Scholl <zack.scholl@gmail.com>
2017-10-06 06:20:30 -0600

    Compress -> Encrypt -> Encode

--- main.go
+++ main.go
@@ -1,7 +1,17 @@
 package main
 
 import (
+	"bytes"
+	"compress/flate"
+	"crypto/aes"
+	"crypto/cipher"
+	"crypto/rand"
+	"crypto/sha256"
+	"encoding/base64"
+	"encoding/hex"
+	"errors"
 	"fmt"
+	"io"
 	"io/ioutil"
 	"os"
 	"time"
@@ -49,10 +59,102 @@ func qrcode(readFile, outputFile string) (err error) {
 	if err != nil {
 		return
 	}
-	q, err := qr.Encode(string(b), qr.L)
+	q, err := qr.Encode(transformTo(b), qr.L)
 	if err != nil {
 		return
 	}
 	err = ioutil.WriteFile(outputFile, q.PNG(), 0644)
 	return
+}
+
+func transformTo(b []byte) string {
+	compressed := compressByte(b)
+	encrypted, _ := encrypt(compressed, "pass")
+	encoded := base64.StdEncoding.EncodeToString(encrypted)
+	fmt.Println(len(b), len(encoded))
+	return encoded
+}
+
+func transformFrom(s string) []byte {
+	decoded, _ := base64.StdEncoding.DecodeString(s)
+	decrypted, _ := decrypt(decoded, "pass")
+	return decompressByte(decrypted)
+}
+
+// compressByte returns a compressed byte slice.
+func compressByte(src []byte) []byte {
+	compressedData := new(bytes.Buffer)
+	compress(src, compressedData, 9)
+	return compressedData.Bytes()
+}
+
+// decompressByte returns a decompressed byte slice.
+func decompressByte(src []byte) []byte {
+	compressedData := bytes.NewBuffer(src)
+	deCompressedData := new(bytes.Buffer)
+	decompress(compressedData, deCompressedData)
+	return deCompressedData.Bytes()
+}
+
+// compress uses flate to compress a byte slice to a corresponding level
+func compress(src []byte, dest io.Writer, level int) {
+	compressor, _ := flate.NewWriter(dest, level)
+	compressor.Write(src)
+	compressor.Close()
+}
+
+// compress uses flate to decompress an io.Reader
+func decompress(src io.Reader, dest io.Writer) {
+	decompressor := flate.NewReader(src)
+	io.Copy(dest, decompressor)
+	decompressor.Close()
+}
+
+func encrypt(plaintext []byte, passphrase string) ([]byte, error) {
+	hasher := sha256.New()
+	hasher.Write([]byte(passphrase))
+	passphrase += hex.EncodeToString(hasher.Sum(nil))
+	key := []byte(passphrase)[:32]
+
+	c, err := aes.NewCipher(key)
+	if err != nil {
+		return nil, err
+	}
+
+	gcm, err := cipher.NewGCM(c)
+	if err != nil {
+		return nil, err
+	}
+
+	nonce := make([]byte, gcm.NonceSize())
+	if _, err = io.ReadFull(rand.Reader, nonce); err != nil {
+		return nil, err
+	}
+
+	return gcm.Seal(nonce, nonce, plaintext, nil), nil
+}
+
+func decrypt(ciphertext []byte, passphrase string) ([]byte, error) {
+	hasher := sha256.New()
+	hasher.Write([]byte(passphrase))
+	passphrase += hex.EncodeToString(hasher.Sum(nil))
+	key := []byte(passphrase)[:32]
+
+	c, err := aes.NewCipher(key)
+	if err != nil {
+		return nil, err
+	}
+
+	gcm, err := cipher.NewGCM(c)
+	if err != nil {
+		return nil, err
+	}
+
+	nonceSize := gcm.NonceSize()
+	if len(ciphertext) < nonceSize {
+		return nil, errors.New("ciphertext too short")
+	}
+
+	nonce, ciphertext := ciphertext[:nonceSize], ciphertext[nonceSize:]
+	return gcm.Open(nil, nonce, ciphertext, nil)
 }
